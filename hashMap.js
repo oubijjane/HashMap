ae40aec4e3dfd3c8fd431a010f0bb9a6c1d52ca8
oubijjane <79411385+oubijjane@users.noreply.github.com>
2024-12-05 11:11:00 +0100

    add new functions . add the function has(key) to chech if the value in the list .add the remove(key) to remove an element .add keys(), values(), entries() to print all the keys and the values as well all the key value pairs . add clear() to remove all element . throw an exeption if you try to access in index out side of the list . the hashMap get resized when reaching or surppasing 75% of it capacity;

--- hashMap.js
+++ hashMap.js
@@ -1,10 +1,11 @@
 import { LinkedList } from "./linked-list.js";
 class hashMap {
-  constructor(capacity = 16) {
+  constructor(capacity = 5) {
     this.loadFactor = 0.75;
     this.capacity = capacity;
     this.buckets = Array(this.capacity).fill(null);
-    this.count = 0;
+    this.arrayCount = 0;
+    this.keyCount = 0;
   }
 
   hash(key) {
@@ -18,13 +19,23 @@ class hashMap {
     return hashCode;
   }
   set(key, value) {
+    if (this.arrayCount >= this.capacity * this.loadFactor) {
+      this.resize();
+    }
+
+    console.log("array: " + this.buckets.length);
     let list = new LinkedList();
     let index = this.hash(key);
+
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
 
     if (!this.buckets[index]) {
       list.append([key, value]);
       this.buckets[index] = list;
-      this.count++;
+      this.arrayCount++;
+      this.keyCount++;
     } else {
       let head = this.buckets[index].getHead();
       while (head) {
@@ -36,12 +47,20 @@ class hashMap {
         head = head.getNextNode();
       }
       this.buckets[index].append([key, value]);
+      this.keyCount++;
     }
+  }
+
+  length() {
+    return this.keyCount;
   }
 
   get(key) {
     let index = this.hash(key);
     let head = this.buckets[index].getHead();
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
     //search for the value using the key which is located at index 0
     while (head) {
       if (head.getValue()[0] === key) {
@@ -51,6 +70,116 @@ class hashMap {
     }
 
     return "not found";
+  }
+  has(key) {
+    let index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    //search for the value using the key which is located at index 0
+    if (!this.buckets[index]) {
+      return false;
+    } else {
+      let toBeChecked = this.buckets[index].getHead();
+      while (toBeChecked) {
+        if (toBeChecked.getValue()[0] === key) {
+          return true;
+        }
+        toBeChecked = toBeChecked.getNextNode();
+      }
+    }
+
+    return false;
+  }
+  remove(key) {
+    let exist = this.has(key);
+    let index = this.hash(key);
+    if (index < 0 || index >= this.buckets.length) {
+      throw new Error("Trying to access index out of bounds");
+    }
+    if (!exist) {
+      return exist;
+    } else {
+      if (this.buckets[index].size() === 1) {
+        this.buckets[index] = null;
+        this.arrayCount--;
+      } else {
+        let toBeRemoved = this.buckets[index].getHead();
+        for (let i = 0; i < this.buckets[index].size(); i++) {
+          if (toBeRemoved.getValue()[0] === key) {
+            this.buckets[index].removeAt(i);
+            this.keyCount -= 1;
+            break;
+          }
+          toBeRemoved = toBeRemoved.getNextNode();
+        }
+      }
+    }
+    return true;
+  }
+
+  clear() {
+    this.buckets = Array(this.capacity).fill(null);
+  }
+
+  keys() {
+    let keys = [];
+    this.buckets.forEach((key) => {
+      if (key) {
+        let head = key.getHead();
+        while (head) {
+          let obtainedKey = head.getValue()[0];
+          keys.push(obtainedKey);
+          head = head.getNextNode();
+        }
+      }
+    });
+    return keys;
+  }
+  values() {
+    let values = [];
+    this.buckets.forEach((value) => {
+      if (value) {
+        let head = value.getHead();
+        while (head) {
+          let obtainedvalue = head.getValue()[1];
+          values.push(obtainedvalue);
+          head = head.getNextNode();
+        }
+      }
+    });
+    return values;
+  }
+  entries() {
+    let entries = [];
+    this.buckets.forEach((value) => {
+      if (value) {
+        let head = value.getHead();
+        while (head) {
+          let obtainedvalue = [head.getValue()[0], head.getValue()[1]];
+          entries.push(obtainedvalue);
+          head = head.getNextNode();
+        }
+      }
+    });
+    return entries;
+  }
+  resize() {
+    this.capacity = this.capacity * 2;
+    let oldbuket = this.buckets;
+
+    this.buckets = Array(this.capacity).fill(null);
+    this.keyCount = 0;
+    this.arrayCount = 0;
+    for (let bucket of oldbuket) {
+      if (bucket != null) {
+        let node = bucket.getHead();
+        while (node) {
+          this.set(node.getValue()[0], node.getValue()[1]); // Re-insert each key-value pair
+          node = node.getNextNode();
+        }
+      }
+    }
   }
 }
 
--- main.js
+++ main.js
@@ -1,7 +1,68 @@
 import { hashMap } from "./hashMap.js";
 
 let test = new hashMap();
-test.set("take", "zakaria");
+/*test.set("take", "zakaria");
 test.set("age", "yes");
-console.log("test " + test.get("take"));
-console.log(test.hash("take"));
+test.set("agkjde", "hello");
+test.set("agde", "dandandan");
+//console.log(test.remove("age"));
+console.log("test " + test.length());
+console.log(test.get("take"));
+console.log(test.entries());
+console.log(test.hash("take")); */
+
+test.set("apple", "red");
+test.set("banana", "yellow");
+test.set("carrot", "orange");
+test.set("dog", "brown");
+test.set("elephant", "gray");
+test.set("frog", "green");
+test.set("grape", "purple");
+test.set("hat", "black");
+test.set("ice cream", "white");
+test.set("jacket", "blue");
+test.set("kite", "pink");
+test.set("uf", "pink");
+test.set("ihf", "pink");
+test.set("ifcr", "pink");
+test.set("icf", "pink");
+test.set("iewf", "pink");
+
+
+console.log("initial length: " + test.length());
+console.log("getting banana value: " + test.get("banana"));
+console.log("remove an element: " + test.remove("ihf"));
+console.log("lenght after removing an element: " + test.length());
+console.log("add a a new element: " + test.set("lion", "golden"));
+console.log("lenght after adding an element: " + test.length());
+console.log("is the hash map has the ice cream element: " + test.has("ice cream"));
+console.log("is the hash map has the tiger element: " + test.has("tiger"));
+console.log("all the keys " + test.keys());
+console.log("all the values " + test.values());
+console.log("entries " + test.entries());
+//console.log("clear all: " + test.clear());
+console.log("all the keys " + test.keys());
+console.log("all the values " + test.values());
+console.log("entries " + test.entries());
+console.log("new cycle");
+console.log("new cycle");
+console.log("new cycle");
+console.log("new cycle");
+
+console.log("initial length: " + test.length());
+//console.log("getting banana value: " + test.get("lion"));
+console.log("remove an element: " + test.remove("hat"));
+console.log("lenght after removing an element: " + test.length());
+console.log("add a a new element: " + test.set("lion", "golden"));
+console.log("lenght after adding an element: " + test.length());
+console.log("is the hash map has the ice cream element: " + test.has("ice cream"));
+console.log("is the hash map has the tiger element: " + test.has("tiger"));
+console.log("all the keys " + test.keys());
+console.log("all the values " + test.values());
+console.log("entries " + test.entries());
+console.log("clear all: " + test.clear());
+console.log("all the keys " + test.keys());
+console.log("all the values " + test.values());
+console.log("entries " + test.entries()); 
+
+
